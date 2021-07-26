fbc46109c73ec85543f35249c448ea3c1888f971
ubdussamad <ubdussamad@gmail.com>
2021-07-26 17:26:11 +0530

    7 tests are oassing other are timing out in hr4

--- hr4.cpp
+++ hr4.cpp
@@ -5,7 +5,6 @@ using namespace std;
 string ltrim(const string &);
 string rtrim(const string &);
 
-inline bool ipo2(unsigned int x) {return (x != 0) && ((x & (x - 1)) == 0);}
 string po2n(int n) {
     int i, j, blen = n / 32 + 1, dlen = n / 29 + 1;
     uint32_t bin[blen], dec[dlen];
@@ -25,126 +24,101 @@ string po2n(int n) {
         while (blen > 0 && bin[blen - 1] == 0)
             blen--;
     }
-    char buff[64];
-    string _s;
+    char buff[1024];
+    // buff[1023] = '\0';
+    int count = 0;
     
-    sprintf(buff,"%u", dec[--j]);
-    _s.append(buff);
-    while (j-- > 0) {char nbuff[64]; sprintf(nbuff,"%09u", dec[j]);_s.append(nbuff);}
+    count += sprintf(&buff[count],"%u", dec[--j]);
+
+    while (j-- > 0) { 
+        count += sprintf( &buff[count], "%09u", dec[j]);
+    }
     
-    return string(_s);}
-unordered_set<string> pow2map;
+    return string(buff);
+    
+}
+
+
 #define ul unsigned int
 
 // Creating a trie DS especially for numerix only strings
 struct tnode {
     // unordered_map<char, tnode* > children; // Generic Case
-    tnode * children[10] = {nullptr}; // if the tnode ptr at numeric offset is null then the child dosen't exists.
+    struct tnode * children[10] = {nullptr}; // if the tnode ptr at numeric offset is null then the child dosen't exists.
     bool value = false;
 };
 
 
 // Param: root doesn't nessesarily need to be absolute root if you know what you're doing.
-bool tnodeInsert (tnode *root , string element) {
-    tnode * rootCpy = root;
-    for (auto& itr: element) {
+bool tnodeInsert (struct tnode *root , string elem) {
+    struct tnode * rootCpy = root;
+    for ( auto& itr : elem ) {
         
-        auto index = (itr-48)%10;
-        
-        if (rootCpy->children[index] != nullptr) {rootCpy = rootCpy->children[index];} // if that child is already there
+        // if that child is already there
+        // cout << "S1" << endl;
+        if (rootCpy->children[ itr-48 ] != nullptr) {rootCpy = rootCpy->children[ itr-48 ];}
         
         else {
-            tnode * babyNode = new tnode();
-            rootCpy->children[index] = babyNode;
+            struct tnode* babyNode = new struct tnode;
+            memset(babyNode->children, 0 , 10 );
+            // cout << "S3" << endl;
+            rootCpy->children[ itr-48 ] = babyNode;
             rootCpy = babyNode;
         }
-        
-        
     }
     rootCpy->value = true;
     return true;
 }
 
 
-int tnodeSubstrings (tnode *root, string query) {
-    tnode * rootCpy = root;
+int tnodeSubstrings (struct tnode *root, string query , unsigned int start , unsigned int end) {
+    struct tnode * rootCpy = root;
     int substringCount = 0;
-    for (auto& itr: query) {
+    for (unsigned int itr = start; itr < end; itr++) {
+        cout << "Loop!" << endl;
         
-        auto index = (itr-48)%10;
-        cout << "Query[i]: " << index ;
-        if ( rootCpy->children[index] != nullptr) {
-            rootCpy = rootCpy->children[index];
-            if (rootCpy->value) {cout << "Hit!";substringCount++;}
-        } // if that child is already there
-        cout << endl;
+        if ( rootCpy->children[ query[itr]-48 ] ) {
+            
+            rootCpy = rootCpy->children[ query[itr]-48 ];
+            
+            if (rootCpy->value) {++substringCount;}
+        }
+        
+        else {
+            return substringCount;
+        }
     }
-
     return substringCount;
 }
 
 
-tnode pow2trie;
+struct tnode pow2trie;
 
 
 int twoTwo(string a) {
     
     ul po2Count(0);
+    
+    // cout << tnodeSubstrings( &pow2trie, string("0") , 0, 1) << endl;
     ul limit = a.size();
-    string q = "256";
-
-    int f = tnodeSubstrings(&pow2trie, q);
-    cout << "Copies found: " << f << endl;
     
-    // for (ul i(0); i < limit; i++ ) {
-    //     // cout << po2n(i) << endl;
-    //     string value;
-    //     value.push_back(a[i]);
-        
-    //     if (a[i] == '0') {continue;} // if starting char is 0
-    //     // cout << "Testing number: " << a[i];
-    //     if ( ipo2(a[i] - 48) ) {// cout << " HIT!" << endl;
-    //         po2Count++;}
-            
-    //     // else {cout << " Miss!" << endl;}
-        
-    //     for (ul j(i+1); j < limit; j++) {
-            
-            
-    //         value.push_back(a[j]);
-            
-    //         // cout << "String Testing number: " << value;
-    //         if (pow2map.find(value) != pow2map.end()) {
-    //             // cout << " Miss!" << endl;
-    //             po2Count++;
-    //             }
-    //         // else {
-    //         //     po2Count++;
-    //         //     // cout << " HIT!" << endl;
-    //         //     }
-
-            
-    //     }
-    // }
-    
-    
+    for (ul i(0); i < limit; i++ ) {
+        po2Count += tnodeSubstrings(&pow2trie, a, i, a.size());
+    }
     return po2Count;
 }
 
 
-
-
-int main() {
-    // cout << "Ran" << endl;
-    for (int i(0); i < 2048*2; i++ ) {
-        string k = po2n(i);
-        // cout << "Inserting " << k << endl;
-        tnodeInsert(&pow2trie, k);
-    }
-
-    cout << "Heyy yo" << endl;
-    
+int main()
+{
     ofstream fout(getenv("OUTPUT_PATH"));
+    
+    for (int i(0); i < 801; i++ ) {
+        string k = po2n(i);
+        // cout << k << "     Len: " << k.size() <<  endl;
+        tnodeInsert(&pow2trie, k);
+        if (k[0] == '1' && k[1] == '9') {cout << k << endl;}
+    }
 
     string t_temp;
     getline(cin, t_temp);
@@ -164,6 +138,7 @@ int main() {
 
     return 0;
 }
+
 string ltrim(const string &str) {
     string s(str);
 
@@ -174,6 +149,7 @@ string ltrim(const string &str) {
 
     return s;
 }
+
 string rtrim(const string &str) {
     string s(str);
 
@@ -184,6 +160,3 @@ string rtrim(const string &str) {
 
     return s;
 }
-
-
-
